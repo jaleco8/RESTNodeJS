5c15f04fcb1705de5c670a4843a470566f99025d
Jesus Leon <jleon@wacoservices.com>
2021-01-29 14:52:58 -0400

    Configuraciones basicas

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({extended: false}));
+
+const places = [
+    {
+        'title': 'JALECO',
+        'description': 'Lorem Ipsum',
+        'address': 'Lorem Ipsum'
+    },
+    {
+        'title': 'JALECO',
+        'description': 'Lorem Ipsum',
+        'address': 'Lorem Ipsum'
+    },
+    {
+        'title': 'JALECO',
+        'description': 'Lorem Ipsum',
+        'address': 'Lorem Ipsum'
+    }
+];
+
 app.get('/', (req, res) => {
-    res.send('Hola mundo...');
+    res.json(places);
 });
+
+app.post('/', (req, res)=>{
+    res.json(req.body);
+});
+
+app.use(express.static('public'));
 
 app.listen(3000, function () {
     console.log('Estoy listo para recibir peticiones');
